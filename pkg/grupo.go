c1487d235ed253d343ddd53094fac7a73bcf96e6
manujurado1 <manujurado1@correo.ugr.es>
2023-01-17 21:12:23 +0100

    Creación método para añadir jugadores. #28
    Se ha creado un método que permite añadir jugadores a un grupo conociendo su nombre, su nivel y si en ese momento
    están disponibles para jugar un partido.
    El jugador será añadido si no existe un jugador con su nombre y su nivel está entre 0 y 100. closes #28

--- pkg/grupo.go
+++ pkg/grupo.go
@@ -21,3 +21,27 @@ func CrearGrupo(Nombre string) (*Grupo, error) {
 	grupo := Grupo{Nombre, JugadoresDisponibles, JugadoresNiveles}
 	return &grupo, nil
 }
+
+// Función que añade un jugador en el map de JugadoresNiveles si ese nombre no tiene una entrada existente y el nivel es correcto
+// y lo inserta en la lista de disponibles en caso de que así se indique
+func (this *Grupo) crearJugador(NombreJugador string, Nivel int, Disponibilidad bool) (bool, error) {
+	var success bool = false
+	_, existe := this.JugadoresNiveles[NombreJugador]
+	if existe == false {
+		if Nivel >= 0 && Nivel <= 100 {
+
+			this.JugadoresNiveles[NombreJugador] = Nivel
+			success = true
+			if Disponibilidad {
+				this.JugadoresDisponibles = append(this.JugadoresDisponibles, NombreJugador)
+			}
+		} else {
+			return success, fmt.Errorf("El nivel del jugador debe estar entre 0 y 100")
+		}
+	} else {
+		return success, fmt.Errorf("Ya existe un jugador con ese nombre")
+	}
+
+	return success, nil
+
+}
--- pkg/grupo_test.go
+++ pkg/grupo_test.go
@@ -18,3 +18,28 @@ func TestCrearGrupo(t *testing.T) {
 	assert.Zero(t, len(grupo.JugadoresDisponibles))
 	assert.Zero(t, len(grupo.JugadoresNiveles))
 }
+
+func TestCrearJugador(t *testing.T) {
+
+	JugadoresNiveles := map[string]int{}
+	grupo := Grupo{Nombre: "GrupoTest", JugadoresNiveles: JugadoresNiveles}
+
+	success, error := grupo.crearJugador("Manuel", 120, false)
+	assert.Equal(t, false, success)
+	assert.Errorf(t, error, "El nivel del jugador debe estar entre 0 y 100")
+
+	success, error = grupo.crearJugador("Manuel", 50, false)
+	assert.Equal(t, true, success)
+	assert.Equal(t, 1, len(grupo.JugadoresNiveles))
+	assert.Equal(t, 0, len(grupo.JugadoresDisponibles))
+
+	success, error = grupo.crearJugador("Jorge", 70, true)
+	assert.Equal(t, true, success)
+	assert.Equal(t, 2, len(grupo.JugadoresNiveles))
+	assert.Equal(t, 1, len(grupo.JugadoresDisponibles))
+
+	success, error = grupo.crearJugador("Manuel", 75, false)
+	assert.Equal(t, false, success)
+	assert.Errorf(t, error, "Ya existe un jugador con ese nombre")
+
+}
