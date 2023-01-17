b1594fcaa097134ce715f0299406029b6abee8f2
manujurado1 <manujurado1@correo.ugr.es>
2023-01-17 20:50:04 +0100

    Creación constructor struct Grupo. #41
    Se ha creado un constructor para la clase grupo en el que solo se pueda elegir el nombre del grupo, inicializando
    el resto de atributos a una lista vacía y un map vacío respectivamente. closes #41

--- pkg/grupo.go
+++ pkg/grupo.go
@@ -1,7 +1,23 @@
 package pkg
 
+import "fmt"
+
 type Grupo struct {
 	Nombre               string
 	JugadoresDisponibles []string
 	JugadoresNiveles     map[string]int
+}
+
+// Constructor que crea un nuevo Grupo con el nombre pasado por parámetro, inicializando la lista de JugadoresDiponibles y el map de JugadoresNiveles vacío.
+func CrearGrupo(Nombre string) (*Grupo, error) {
+
+	JugadoresDisponibles := []string{}
+	JugadoresNiveles := make(map[string]int)
+
+	if Nombre == "" {
+		return nil, fmt.Errorf("El nombre del grupo no puede ser un string vacío")
+	}
+
+	grupo := Grupo{Nombre, JugadoresDisponibles, JugadoresNiveles}
+	return &grupo, nil
 }
--- pkg/grupo_test.go
+++ pkg/grupo_test.go
@@ -1,1 +1,20 @@
 package pkg
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCrearGrupo(t *testing.T) {
+
+	grupo, error := CrearGrupo("")
+	assert.Nil(t, grupo)
+	assert.Errorf(t, error, "El nombre del grupo no puede ser un string vacío")
+
+	grupo, error = CrearGrupo("Grupo1")
+	assert.Nil(t, error)
+	assert.Equal(t, "Grupo1", grupo.Nombre)
+	assert.Zero(t, len(grupo.JugadoresDisponibles))
+	assert.Zero(t, len(grupo.JugadoresNiveles))
+}
