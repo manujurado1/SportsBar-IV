ba1e37b4b9b60e0465e4ff795546533b9903fcaa
manujurado1 <manujurado1@correo.ugr.es>
2023-01-20 19:48:32 +0100

    Crear constructor clase grupo. #51
    Se ha creado un constructor que permita solo elegir el nombre del grupo a la hora de crearlo,
     inicializando vacíos los otros 2 atributos.
    Con esto se consigue dejar la gestión de estos atributos a funciones específicas para gestionar los requisitos
     de cada atributo y así evitar introducir valores inválidos
    . closes #51

--- pkg/modelos/grupo.go
+++ pkg/modelos/grupo.go
@@ -1,7 +1,23 @@
 package modelos
 
+import "fmt"
+
 type Grupo struct {
 	Nombre                  string
 	JugadoresDisponibilidad map[string]bool
 	JugadoresNiveles        map[string]int
+}
+
+// Constructor que crea un nuevo Grupo con el nombre pasado por parámetro, inicializando  el map de JugadoresNiveles y el de Jugadores Disponibilidad vacíos.
+func CrearGrupo(Nombre string) (*Grupo, error) {
+
+	JugadoresDisponibilidad := make(map[string]bool)
+	JugadoresNiveles := make(map[string]int)
+
+	if Nombre == "" {
+		return nil, fmt.Errorf("El nombre del grupo no puede ser un string vacío")
+	}
+
+	grupo := Grupo{Nombre, JugadoresDisponibilidad, JugadoresNiveles}
+	return &grupo, nil
 }
