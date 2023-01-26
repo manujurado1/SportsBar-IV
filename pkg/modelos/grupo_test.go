3aa5d8591e68dc0af6857c76ab2559d17d9c2b8e
manujurado1 <manujurado1@correo.ugr.es>
2023-01-26 18:12:05 +0100

    Eliminar test constructor closes #51

--- pkg/modelos/grupo_test.go
+++ pkg/modelos/grupo_test.go
@@ -6,18 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCrearGrupo(t *testing.T) {
-	grupo, error := CrearGrupo("")
-	assert.Nil(t, grupo)
-	assert.Errorf(t, error, "El nombre del grupo no puede ser un string vacío")
-
-	grupo, error = CrearGrupo("Grupo1")
-	assert.Nil(t, error)
-	assert.Equal(t, "Grupo1", grupo.Nombre)
-	assert.Zero(t, len(grupo.JugadoresDisponibilidad))
-	assert.Zero(t, len(grupo.JugadoresNiveles))
-}
-
 func TestCrearJugador(t *testing.T) {
 
 	JugadoresNiveles := map[string]int{}
