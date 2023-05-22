154c7ccb6aceb569582e624927a45f3b8fe9356c
manujurado1 <manujurado1@correo.ugr.es>
2023-05-22 18:31:04 +0200

    Modificar niveles tras partido. closes #87
    Se han creado una serie de criterios de modificación del nivel en función del resultado (closes #86) y se han aplicado a la hora de finalizar un partido.

--- pkg/modelos/estadisticas_jugador.go
+++ pkg/modelos/estadisticas_jugador.go
@@ -8,3 +8,13 @@ type EstadisticasJugador struct {
 func (this *EstadisticasJugador) setDisponibilidad(Disponibilidad bool) {
 	this.Disponibilidad = Disponibilidad
 }
+
+func (this *EstadisticasJugador) setNivel(Nivel int) {
+	if Nivel <= 0 {
+		this.Nivel = 1
+	} else if Nivel > 100 {
+		this.Nivel = 100
+	} else {
+		this.Nivel = uint(Nivel)
+	}
+}
