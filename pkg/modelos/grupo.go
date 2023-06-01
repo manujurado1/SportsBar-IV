f365cad2db13b508172bddf7245b45fb9afe7b2e
manujurado1 <manujurado1@correo.ugr.es>
2023-06-01 16:49:11 +0200

    Añadir diseño de errores. closes #110
    Se han creado los posibles errores que pueden suceder en la aplicación.

--- pkg/modelos/grupo.go
+++ pkg/modelos/grupo.go
@@ -1,5 +1,14 @@
 package modelos
 
+import "errors"
+
+var (
+	ErrorFaltaNombre                  = errors.New("Se necesita introducir un nombre para crear el grupo")
+	ErrorAmigoRepetido                = errors.New("Ya existe un amigo con ese nombre en el grupo de amigos")
+	ErrorAmigosDisponiblesInadecuados = errors.New("Para crear 2 equipos debe haber un número par de amigos disponibles igual o superior a 10")
+	ErrorImposibilidadEquipos         = errors.New("Ha sido imposible crear 2 equipos igualados con los amigos disponibles")
+)
+
 type GrupoAmigos struct {
 	Nombre string
 	Amigos map[string]*Amigo
