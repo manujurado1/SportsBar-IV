28659d8ea19ff1c3a920aa022286188f9db03df1
manujurado1 <manujurado1@correo.ugr.es>
2023-06-05 18:20:00 +0200

    Crear estructura de datos nivel. closes #112
    Se ha creado una estructura de datos que resuelva el problema. Se ha optado por un rango de números del 0 al 10, que permita el 5 como valor medio.
    Se ha añadido un constructor y métodos para aumentar/disminuir el nivel, manteniendo los rangos máximo y mínimo.

--- pkg/modelos/nivel.go
+++ pkg/modelos/nivel.go
@@ -1,18 +1,44 @@
 package modelos
 
+import (
+	"fmt"
+)
+
 type Nivel int
 
 const (
-	NivelMinimo Nivel = 1
+	NivelMinimo Nivel = 0
 	NivelMaximo Nivel = 10
+	NivelMedio  Nivel = 5
 )
 
-func (n *Nivel) Set(valor Nivel) {
-	if valor < NivelMinimo {
-		*n = NivelMinimo
-	} else if valor > NivelMaximo {
+var (
+	ErrorNivelFueraDeRango = fmt.Errorf("El nivel de un jugador debe estar en el rango %d - %d", NivelMinimo, NivelMaximo)
+)
+
+func NuevoNivel(valor Nivel) (*Nivel, error) {
+	if valor < NivelMinimo || valor > NivelMaximo {
+		return nil, ErrorNivelFueraDeRango
+	}
+
+	nivel := Nivel(valor)
+	return &nivel, nil
+}
+
+func (n *Nivel) AumentarNivel(cantidad Nivel) {
+	nuevoNivel := *n + cantidad
+	if nuevoNivel > NivelMaximo {
 		*n = NivelMaximo
 	} else {
-		*n = valor
+		*n = nuevoNivel
+	}
+}
+
+func (n *Nivel) DisminuirNivel(cantidad Nivel) {
+	nuevoNivel := *n - cantidad
+	if nuevoNivel < NivelMinimo {
+		*n = NivelMinimo
+	} else {
+		*n = nuevoNivel
 	}
 }
---
+++ pkg/modelos/nivel_test.go
@@ -0,0 +1,51 @@
+package modelos
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetNivel(t *testing.T) {
+
+	//Caso asignación incorrecta
+	nivel, error := NuevoNivel(12)
+	assert.Errorf(t, error, "El nivel de un jugador debe estar en el rango 0 - 10")
+	assert.Nil(t, nivel)
+
+	//Caso asignación correcta
+	nivel2, error2 := NuevoNivel(5)
+	assert.Nil(t, error2)
+	assert.Equal(t, Nivel(5), *nivel2)
+
+}
+
+func TestAumentarNivel(t *testing.T) {
+
+	//Aumentar dentro de los límites
+	nivel, error := NuevoNivel(5)
+	nivel.AumentarNivel(3)
+	assert.Nil(t, error)
+	assert.Equal(t, Nivel(8), *nivel)
+
+	//Aumentamos más allá de los límites
+	nivel2, error2 := NuevoNivel(8)
+	nivel2.AumentarNivel(5)
+	assert.Nil(t, error2)
+	assert.Equal(t, NivelMaximo, *nivel2)
+}
+
+func TestDisminuirNivel(t *testing.T) {
+
+	//Disminuir dentro de los límites
+	nivel, error := NuevoNivel(5)
+	nivel.DisminuirNivel(3)
+	assert.Nil(t, error)
+	assert.Equal(t, Nivel(2), *nivel)
+
+	//Aumentamos más allá de los límites
+	nivel2, error2 := NuevoNivel(3)
+	nivel2.DisminuirNivel(5)
+	assert.Nil(t, error2)
+	assert.Equal(t, NivelMinimo, *nivel2)
+}
