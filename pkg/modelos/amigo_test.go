002b4bbf7953554cbf6d0806af0d394074986953
manujurado1 <manujurado1@correo.ugr.es>
2023-06-13 18:42:22 +0200

    Cambiar nombre test. #114

--- pkg/modelos/amigo_test.go
+++ pkg/modelos/amigo_test.go
@@ -15,8 +15,8 @@ func TestNuevoAmigo(t *testing.T) {
 	assert.Equal(t, Amigo{}, amigo)
 
 	//Caso correcto, comprobando que 2 personas con el mismo nick tienen identificadores diferentes
-	amigo2, error2 := NewAmigo("Juanito", time.Date(1999, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
-	amigo3, error3 := NewAmigo("Juanito", time.Date(2001, time.January, 22, 0, 0, 0, 0, time.Now().Location()))
+	amigo2, error2 := NewAmigo("Jose", time.Date(1999, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
+	amigo3, error3 := NewAmigo("Jose", time.Date(2001, time.January, 22, 0, 0, 0, 0, time.Now().Location()))
 
 	assert.Nil(t, error2)
 	assert.Nil(t, error3)
