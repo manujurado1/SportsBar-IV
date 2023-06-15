a57fb8bff666a86fe334f9eeca604b8678d672e0
manujurado1 <manujurado1@correo.ugr.es>
2023-06-15 14:33:08 +0200

    Fix problemas de detalle. closes #120

--- pkg/modelos/grupo.go
+++ pkg/modelos/grupo.go
@@ -5,104 +5,107 @@ import (
 	"time"
 )
 
-var (
-	ErrorNombreGrupoVacio = fmt.Errorf("Un grupo no puede tener como nombre un string vacío")
-	ErrorAmigoDuplicado   = fmt.Errorf("Ya existe un amigo con ese identificador")
-	ErrorAmigoInexistente = fmt.Errorf("No existe el amigo indicado dentro del grupo de amigos")
-)
-
 const (
 	DisponibilidadPorDefecto bool = true
 )
 
-type GrupoAmigos struct {
-	NombreDelGrupo       string
-	ListaAmigos          []Amigo
-	NivelAmigos          map[string]Nivel
-	DisponibilidadAmigos map[string]bool
+var (
+	ErrorNombreGrupoVacio = fmt.Errorf("Un grupo no puede tener como nombre un string vacío")
+	ErrorAmigoDuplicado   = fmt.Errorf("Ya existe un amigo con ese identificador")
+	ErrorAmigoInexistente = fmt.Errorf("No existe el amigo indicado dentro del grupo de amigos")
+	ErrorListaAmigosVacia = fmt.Errorf("No se puede crear un grupo de amigos con una lista de amigos vacía")
+)
+
+var EstadoAmigoPorDefecto EstadoAmigo = EstadoAmigo{
+	Nivel:      NivelPorOmision,
+	Disponible: DisponibilidadPorDefecto,
 }
 
-func NewGrupoAmigos(nombre string) (*GrupoAmigos, error) {
+type EstadoAmigo struct {
+	Nivel      Nivel
+	Disponible bool
+}
+type GrupoAmigos struct {
+	NombreDelGrupo             string
+	ListaAmigos                []Amigo
+	NivelYDisponibilidadAmigos map[string]EstadoAmigo
+}
+
+func NewGrupoAmigos(nombre string, listaAmigos []Amigo) (*GrupoAmigos, error) {
 	if nombre == "" {
 		return nil, ErrorNombreGrupoVacio
 	}
+	if len(listaAmigos) == 0 {
+		return nil, ErrorListaAmigosVacia
+	}
+
+	estadosAmigos := map[string]EstadoAmigo{}
+
+	for _, amigo := range listaAmigos {
+		estadosAmigos[amigo.ObtenerId()] = EstadoAmigoPorDefecto
+	}
 
 	return &GrupoAmigos{
-		NombreDelGrupo:       nombre,
-		ListaAmigos:          []Amigo{},
-		NivelAmigos:          make(map[string]Nivel),
-		DisponibilidadAmigos: make(map[string]bool),
+		NombreDelGrupo:             nombre,
+		ListaAmigos:                listaAmigos,
+		NivelYDisponibilidadAmigos: estadosAmigos,
 	}, nil
 }
 
-func (g *GrupoAmigos) CrearAmigoYAniadirAlGrupo(nickAmigo string, fechaNacimiento time.Time) (bool, error) {
-	success := false
-	Amigo, errorAmigo := NewAmigo(nickAmigo, fechaNacimiento)
+func (g *GrupoAmigos) CrearAmigoYAniadirAlGrupo(nickAmigo string, fechaNacimiento time.Time) error {
+	amigo, errorAmigo := NewAmigo(nickAmigo, fechaNacimiento)
 
 	if errorAmigo != nil {
-		return success, errorAmigo
+		return errorAmigo
 	}
 
-	if g.ExisteAmigo(Amigo) {
-		return success, ErrorAmigoDuplicado
+	_, existe := g.NivelYDisponibilidadAmigos[amigo.ObtenerId()]
+
+	if existe {
+		return ErrorAmigoDuplicado
 	}
 
-	g.ListaAmigos = append(g.ListaAmigos, Amigo)
-	g.NivelAmigos[Amigo.ObtenerId()] = NewNivel()
-	g.DisponibilidadAmigos[Amigo.ObtenerId()] = DisponibilidadPorDefecto
-	success = true
+	g.ListaAmigos = append(g.ListaAmigos, amigo)
+	g.NivelYDisponibilidadAmigos[amigo.ObtenerId()] = EstadoAmigoPorDefecto
 
-	return success, nil
+	return nil
 }
 
-func (g *GrupoAmigos) ExisteAmigo(amigo Amigo) bool {
-	existe := false
-	idAmigo := amigo.ObtenerId()
+func (g *GrupoAmigos) CambiarDisponibilidadAmigo(amigo Amigo, disponibilidad bool) error {
+	estado, existe := g.NivelYDisponibilidadAmigos[amigo.ObtenerId()]
 
-	for _, amigo := range g.ListaAmigos {
-		if amigo.ObtenerId() == idAmigo {
-			existe = true
-		}
+	if !existe {
+		return ErrorAmigoInexistente
 	}
-	return existe
 
+	estado.Disponible = disponibilidad
+	g.NivelYDisponibilidadAmigos[amigo.ObtenerId()] = estado
+
+	return nil
 }
 
-func (g *GrupoAmigos) CambiarDisponibilidadAmigo(amigo Amigo, disponibilidad bool) (bool, error) {
-	success := false
+func (g *GrupoAmigos) AumentarNivelAmigo(amigo Amigo) error {
+	estado, existe := g.NivelYDisponibilidadAmigos[amigo.ObtenerId()]
 
-	if !g.ExisteAmigo(amigo) {
-		return success, ErrorAmigoInexistente
+	if !existe {
+		return ErrorAmigoInexistente
 	}
 
-	g.DisponibilidadAmigos[amigo.ObtenerId()] = disponibilidad
-	success = true
+	estado.Nivel = estado.Nivel.AumentarNivel()
+	g.NivelYDisponibilidadAmigos[amigo.ObtenerId()] = estado
 
-	return success, nil
+	return nil
 }
 
-func (g *GrupoAmigos) AumentarNivelAmigo(amigo Amigo) (bool, error) {
-	success := false
+func (g *GrupoAmigos) DisminuirNivelAmigo(amigo Amigo) error {
+	estado, existe := g.NivelYDisponibilidadAmigos[amigo.ObtenerId()]
 
-	if !g.ExisteAmigo(amigo) {
-		return success, ErrorAmigoInexistente
+	if !existe {
+		return ErrorAmigoInexistente
 	}
 
-	g.NivelAmigos[amigo.ObtenerId()] = g.NivelAmigos[amigo.ObtenerId()].AumentarNivel()
-	success = true
+	estado.Nivel = estado.Nivel.DisminuirNivel()
+	g.NivelYDisponibilidadAmigos[amigo.ObtenerId()] = estado
 
-	return success, nil
-}
-
-func (g *GrupoAmigos) DisminuirNivelAmigo(amigo Amigo) (bool, error) {
-	success := false
-
-	if !g.ExisteAmigo(amigo) {
-		return success, ErrorAmigoInexistente
-	}
-
-	g.NivelAmigos[amigo.ObtenerId()] = g.NivelAmigos[amigo.ObtenerId()].DisminuirNivel()
-	success = true
-
-	return success, nil
+	return nil
 }
--- pkg/modelos/grupo_test.go
+++ pkg/modelos/grupo_test.go
@@ -9,116 +9,140 @@ import (
 
 func TestNewGrupoAmigos(t *testing.T) {
 
+	amigo, _ := NewAmigo("Javi", time.Date(1999, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
+	amigo2, _ := NewAmigo("Migue", time.Date(2001, time.January, 22, 0, 0, 0, 0, time.Now().Location()))
+	listaAmigos := []Amigo{amigo, amigo2}
+	estadoAmigos := map[string]EstadoAmigo{}
+	estadoAmigos[amigo.ObtenerId()] = EstadoAmigoPorDefecto
+	estadoAmigos[amigo2.ObtenerId()] = EstadoAmigoPorDefecto
+
 	//Caso incorrecto: Nombre vacío
-	grupo, errorGrupo := NewGrupoAmigos("")
+	grupo, errorGrupo := NewGrupoAmigos("", listaAmigos)
 
 	assert.EqualError(t, errorGrupo, "Un grupo no puede tener como nombre un string vacío")
 	assert.Nil(t, grupo)
 
-	//Caso correcto
-	grupo2, errorGrupo2 := NewGrupoAmigos("Prueba")
-	grupoEsperado := GrupoAmigos{"Prueba", []Amigo{}, map[string]Nivel{}, map[string]bool{}}
+	//Caso incorrecto: Lista de amigos vacía
+	grupo2, errorGrupo2 := NewGrupoAmigos("Nombre", []Amigo{})
 
-	assert.Nil(t, errorGrupo2)
-	assert.Equal(t, grupoEsperado, *grupo2)
+	assert.EqualError(t, errorGrupo2, "No se puede crear un grupo de amigos con una lista de amigos vacía")
+	assert.Nil(t, grupo2)
+
+	//Caso correcto
+	grupo3, errorGrupo3 := NewGrupoAmigos("Prueba", listaAmigos)
+	grupoEsperado := GrupoAmigos{"Prueba", listaAmigos, estadoAmigos}
+
+	assert.Nil(t, errorGrupo3)
+	assert.Equal(t, grupoEsperado, *grupo3)
 }
 
 func TestCrearAmigoYAniadirAlGrupo(t *testing.T) {
 
+	amigo, _ := NewAmigo("Javi", time.Date(1999, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
+	amigo2, _ := NewAmigo("Migue", time.Date(2001, time.January, 22, 0, 0, 0, 0, time.Now().Location()))
+	listaAmigos := []Amigo{amigo, amigo2}
+
 	//Caso incorrecto por nombre amigo vacío
-	grupo, _ := NewGrupoAmigos("Prueba")
-	success, errorAmigo := grupo.CrearAmigoYAniadirAlGrupo("", time.Date(1990, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
-	assert.False(t, success)
+	grupo, _ := NewGrupoAmigos("Prueba", listaAmigos)
+	errorAmigo := grupo.CrearAmigoYAniadirAlGrupo("", time.Date(1990, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
 	assert.EqualError(t, errorAmigo, "El nick de un amigo no puede ser un string vacío")
 
 	//Caso correcto añadiendo 2 amigos con el mismo nombre
-	grupo2, _ := NewGrupoAmigos("Prueba2")
+	grupo2, _ := NewGrupoAmigos("Prueba2", listaAmigos)
 
-	success, errorAmigo = grupo2.CrearAmigoYAniadirAlGrupo("Jose", time.Date(1990, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
-	assert.True(t, success)
+	errorAmigo = grupo2.CrearAmigoYAniadirAlGrupo("Jose", time.Date(1990, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
 	assert.Nil(t, errorAmigo)
 
-	success, errorAmigo = grupo2.CrearAmigoYAniadirAlGrupo("Jose", time.Date(1990, time.August, 19, 0, 0, 0, 0, time.Now().Location()))
-	assert.True(t, success)
+	errorAmigo = grupo2.CrearAmigoYAniadirAlGrupo("Jose", time.Date(1990, time.August, 19, 0, 0, 0, 0, time.Now().Location()))
 	assert.Nil(t, errorAmigo)
-	assert.Equal(t, 2, len(grupo2.ListaAmigos))
+	assert.Equal(t, 4, len(grupo2.ListaAmigos))
 }
 
 func TestModificarDisponibilidadAmigo(t *testing.T) {
 
+	amigo, _ := NewAmigo("Javi", time.Date(1999, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
+	amigo2, _ := NewAmigo("Migue", time.Date(2001, time.January, 22, 0, 0, 0, 0, time.Now().Location()))
+	listaAmigos := []Amigo{amigo, amigo2}
+
 	//Caso incorrecto intentando cambiar disponibilidad de un amigo que no está en el grupo
-	grupo, _ := NewGrupoAmigos("Prueba")
-	amigo, _ := NewAmigo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
+	grupo, _ := NewGrupoAmigos("Prueba", listaAmigos)
+	amigoNuevo, _ := NewAmigo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
 
-	success, errorDisponibilidad := grupo.CambiarDisponibilidadAmigo(amigo, false)
+	errorDisponibilidad := grupo.CambiarDisponibilidadAmigo(amigoNuevo, false)
 
-	assert.False(t, success)
 	assert.EqualError(t, errorDisponibilidad, ErrorAmigoInexistente.Error())
 
 	//Caso correcto
-	_, _ = grupo.CrearAmigoYAniadirAlGrupo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
-	amigo = grupo.ListaAmigos[0]
-	success, errorDisponibilidad = grupo.CambiarDisponibilidadAmigo(amigo, false)
+	_ = grupo.CrearAmigoYAniadirAlGrupo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
+	amigoNuevo = grupo.ListaAmigos[2]
+	errorDisponibilidad = grupo.CambiarDisponibilidadAmigo(amigoNuevo, false)
+	estado := grupo.NivelYDisponibilidadAmigos[amigoNuevo.ObtenerId()]
 
-	assert.True(t, success)
 	assert.Nil(t, errorDisponibilidad)
-	assert.False(t, grupo.DisponibilidadAmigos[amigo.ObtenerId()])
+	assert.False(t, estado.Disponible)
 
 }
 
 func TestAumentarNivelAmigo(t *testing.T) {
+	amigo, _ := NewAmigo("Javi", time.Date(1999, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
+	amigo2, _ := NewAmigo("Migue", time.Date(2001, time.January, 22, 0, 0, 0, 0, time.Now().Location()))
+	listaAmigos := []Amigo{amigo, amigo2}
+
 	//Caso incorrecto intentando cambiar disponibilidad de un amigo que no está en el grupo
-	grupo, _ := NewGrupoAmigos("Prueba")
-	amigo, _ := NewAmigo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
+	grupo, _ := NewGrupoAmigos("Prueba", listaAmigos)
+	amigoNuevo, _ := NewAmigo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
 
-	success, errorNivel := grupo.AumentarNivelAmigo(amigo)
+	errorNivel := grupo.AumentarNivelAmigo(amigoNuevo)
 
-	assert.False(t, success)
 	assert.EqualError(t, errorNivel, ErrorAmigoInexistente.Error())
 
 	//Caso correcto sin alcanzar el límite
-	_, _ = grupo.CrearAmigoYAniadirAlGrupo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
-	amigo = grupo.ListaAmigos[0]
-	success, errorNivel = grupo.AumentarNivelAmigo(amigo)
+	_ = grupo.CrearAmigoYAniadirAlGrupo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
+	amigoAniadido := grupo.ListaAmigos[2]
+	errorNivel = grupo.AumentarNivelAmigo(amigoAniadido)
 
-	assert.True(t, success)
 	assert.Nil(t, errorNivel)
-	assert.Greater(t, grupo.NivelAmigos[amigo.ObtenerId()], NivelPorOmision)
+	assert.Greater(t, grupo.NivelYDisponibilidadAmigos[amigoAniadido.ObtenerId()].Nivel, NivelPorOmision)
 
 	//Caso correcto alcanzando el límite
 
-	grupo.NivelAmigos[amigo.ObtenerId()] = NivelMaximo
-	success, errorNivel = grupo.AumentarNivelAmigo(amigo)
-	assert.True(t, success)
+	estado := grupo.NivelYDisponibilidadAmigos[amigoAniadido.ObtenerId()]
+	estado.Nivel = NivelMaximo
+	grupo.NivelYDisponibilidadAmigos[amigoAniadido.ObtenerId()] = estado
+	errorNivel = grupo.AumentarNivelAmigo(amigoAniadido)
 	assert.Nil(t, errorNivel)
-	assert.Equal(t, grupo.NivelAmigos[amigo.ObtenerId()], NivelMaximo)
+	assert.Equal(t, grupo.NivelYDisponibilidadAmigos[amigoAniadido.ObtenerId()].Nivel, NivelMaximo)
 }
 
 func TestDisminuirNivelAmigo(t *testing.T) {
+
+	amigo, _ := NewAmigo("Javi", time.Date(1999, time.August, 17, 0, 0, 0, 0, time.Now().Location()))
+	amigo2, _ := NewAmigo("Migue", time.Date(2001, time.January, 22, 0, 0, 0, 0, time.Now().Location()))
+	listaAmigos := []Amigo{amigo, amigo2}
+
 	//Caso incorrecto intentando cambiar disponibilidad de un amigo que no está en el grupo
-	grupo, _ := NewGrupoAmigos("Prueba")
-	amigo, _ := NewAmigo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
+	grupo, _ := NewGrupoAmigos("Prueba", listaAmigos)
+	amigoNuevo, _ := NewAmigo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
 
-	success, errorNivel := grupo.DisminuirNivelAmigo(amigo)
+	errorNivel := grupo.AumentarNivelAmigo(amigoNuevo)
 
-	assert.False(t, success)
 	assert.EqualError(t, errorNivel, ErrorAmigoInexistente.Error())
 
 	//Caso correcto sin alcanzar el límite
-	_, _ = grupo.CrearAmigoYAniadirAlGrupo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
-	amigo = grupo.ListaAmigos[0]
-	success, errorNivel = grupo.DisminuirNivelAmigo(amigo)
+	_ = grupo.CrearAmigoYAniadirAlGrupo("Migue", time.Date(1999, time.April, 27, 0, 0, 0, 0, time.Now().Location()))
+	amigoAniadido := grupo.ListaAmigos[2]
+	errorNivel = grupo.DisminuirNivelAmigo(amigoAniadido)
 
-	assert.True(t, success)
 	assert.Nil(t, errorNivel)
-	assert.Less(t, grupo.NivelAmigos[amigo.ObtenerId()], NivelPorOmision)
+	assert.Less(t, grupo.NivelYDisponibilidadAmigos[amigoAniadido.ObtenerId()].Nivel, NivelPorOmision)
 
 	//Caso correcto alcanzando el límite
 
-	grupo.NivelAmigos[amigo.ObtenerId()] = NivelMinimo
-	success, errorNivel = grupo.DisminuirNivelAmigo(amigo)
-	assert.True(t, success)
+	estado := grupo.NivelYDisponibilidadAmigos[amigoAniadido.ObtenerId()]
+	estado.Nivel = NivelMinimo
+	grupo.NivelYDisponibilidadAmigos[amigoAniadido.ObtenerId()] = estado
+	errorNivel = grupo.DisminuirNivelAmigo(amigoAniadido)
 	assert.Nil(t, errorNivel)
-	assert.Equal(t, grupo.NivelAmigos[amigo.ObtenerId()], NivelMinimo)
+	assert.Equal(t, grupo.NivelYDisponibilidadAmigos[amigoAniadido.ObtenerId()].Nivel, NivelMinimo)
 
 }
